bcc9607b006f527066c3541fd6bcf28da97d6bbe
Ashley Allen <mail@ashallendesign.co.uk>
2021-04-24 17:57:24 +0100

    Tidied up code.

--- src/Traits/HasSpinner.php
+++ src/Traits/HasSpinner.php
@@ -20,15 +20,13 @@ trait HasSpinner
      */
     public function withSpinner(callable $closure, string $outputText = ''): bool
     {
-        $cacheKey = 'spinner_'.time().'_'.Str::random(30);
-
         $section = (new ConsoleOutput)->section();
 
         Fork::new()
-            ->before(fn(): bool => Cache::put($cacheKey, true))
+            ->before(fn(): bool => $this->startSpinner())
             ->run(
-                $this->spin($cacheKey, $section, $outputText),
-                $this->runCallable($cacheKey, $closure)
+                $this->spin($section, $outputText),
+                $this->runCallable($closure)
             );
 
         return true;
@@ -38,15 +36,14 @@ trait HasSpinner
      * Start the spinner and keep going until we can detect in the
      * state that it should stopped.
      *
-     * @param $cacheKey
      * @param $section
      * @param  string  $outputText
      * @return callable
      */
-    private function spin($cacheKey, $section, string $outputText): callable
+    private function spin($section, string $outputText): callable
     {
-        return function () use ($outputText, $cacheKey, $section): void {
-            while (Cache::get($cacheKey)) {
+        return function () use ($outputText, $section): void {
+            while ($this->isSpinning()) {
                 foreach ($this->frames as $frame) {
                     $section->overwrite($frame.' '.$outputText);
                     usleep(100000);
@@ -61,16 +58,62 @@ trait HasSpinner
      * Run the closure that was passed in by the user. After it has
      * finished running, update the state to stop the spinner.
      *
-     * @param $cacheKey
      * @param $closure
      * @return callable
      */
-    private function runCallable($cacheKey, $closure): callable
+    private function runCallable($closure): callable
     {
-        return static function () use ($closure, $cacheKey): void {
+        return function () use ($closure): void {
             $closure();
 
-            Cache::put($cacheKey, false);
+            $this->stopSpinner();
         };
+    }
+
+    /**
+     * Start the spinner.
+     *
+     * @return bool
+     */
+    private function startSpinner(): bool
+    {
+        return Cache::put($this->cacheKey(), true);
+    }
+
+    /**
+     * Stop the spinner.
+     *
+     * @return bool
+     */
+    private function stopSpinner(): bool
+    {
+        return Cache::put($this->cacheKey(), false);
+    }
+
+    /**
+     * Determine whether the spinner is spinning and should continue.
+     *
+     * @return bool
+     */
+    private function isSpinning(): bool
+    {
+        return Cache::get($this->cacheKey());
+    }
+
+    /**
+     * Build and return a cache key that can be used to fetch
+     * and update the spinner's state.
+     *
+     * @return bool
+     */
+    private function cacheKey(): bool
+    {
+        static $cacheKey;
+
+        if ($cacheKey) {
+            return $cacheKey;
+        }
+
+        return $cacheKey = 'spinner_'.time().'_'.Str::random(30);
     }
 }
